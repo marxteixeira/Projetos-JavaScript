96a8eb8750be0f62d77e1a0543fa5d680234960f
Marx Teixeira <marxtts@gmail.com>
2021-12-01 22:21:43 -0300

    commit primeiros projetos

---
+++ Verificação de Multas - DETRAN/ex010/ex008.js
@@ -0,0 +1,7 @@
+var vel = 55
+console.log(`O seu carro está a ${vel}km/h`)
+if(vel > 60){
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+
+console.log('Dirija sempre usando o cinto de segurança.')
---
+++ Verificação de Multas - DETRAN/ex010/ex009.js
@@ -0,0 +1,8 @@
+
+var país = 'Peru'
+console.log(`Vivendo no ${país}`)
+if (país == 'Brasil'){
+    console.log ('Você é Brasileiro') 
+} else {
+console.log ('Você é Estrangeiro')
+}
